a7a0f3599ce1ea00f7f49a5f657d3151df0fd7d0
2Grey8 <142738191+2Grey8@users.noreply.github.com>
2024-09-03 14:09:56 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import 'normalize.css'
+import './styles/main.scss'
+// core version + navigation, pagination modules:
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// init Swiper:
+const swiper = new Swiper('.tour__slider', {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    pagination: {
+        el: ".swiper-pagination",
+    },
+
+    // configure Swiper to use modules
+    modules: [Navigation, Pagination],
+});
+
+
